92089dc50d830f8b6f1e8fd465c64df5f2b12393
Igor Bologov <peteshkus@gmail.com>
2025-12-08 13:34:25 +0200

    feat(seatpage) websocket realtime update of reservations done

--- cinema-client/src/util/baseURL.ts
+++ cinema-client/src/util/baseURL.ts
@@ -1,33 +1,18 @@
-const API_BASE_URL =
+export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
-// export const API_ENDPOINTS = {
-//   test: `${API_BASE_URL}/api/test`,
-//   dbHealth: `${API_BASE_URL}/api/db-health`,
-//   changePassword: `${API_BASE_URL}/auth/change-password`,
-//   login: `${API_BASE_URL}/auth/login`,
-//   protectedDashboard: `${API_BASE_URL}/admin/dashboard`,
-//   cinemas: `${API_BASE_URL}/admin/cinemas`,
-//   cities: `${API_BASE_URL}/admin/cities`,
-//   cinemasByCity: (cityUid: string) =>
-//     `${API_BASE_URL}/admin/cinemas/${cityUid}`,
-//   hallsByCinema: (cinemaUid: string) =>
-//     `${API_BASE_URL}/admin/halls/${cinemaUid}`,
-//   addHall: `${API_BASE_URL}/admin/halls`,
-//   hallDetails: (hallUid: string) => `${API_BASE_URL}/admin/halls/${hallUid}`,
-//   //for viewing single hall
-//   hallDetail: (hallUid: string) => `${API_BASE_URL}/admin/hall/${hallUid}`,
-//   halls: `${API_BASE_URL}/admin/halls`,
-//   movies: `${API_BASE_URL}/admin/movies`,
-// };
-
+/**
+ * BASE URL for WebSockets and HTTP requests
+ */
 export const API_ENDPOINTS = {
+  base: API_BASE_URL, // <-- НУЖНО ДЛЯ WEBSOCKET
+
   cinemas: `${API_BASE_URL}/api/cinemas`,
   cities: `${API_BASE_URL}/api/cities`,
-  movies: `${API_BASE_URL}/api/movies`, 
-  showtimes: `${API_BASE_URL}/api/client/showtimes`,   
-  showtime: `${API_BASE_URL}/api/client/showtimes`,    
+  movies: `${API_BASE_URL}/api/movies`,
+  showtimes: `${API_BASE_URL}/api/client/showtimes`,
+  showtime: `${API_BASE_URL}/api/client/showtimes`,
   seats: `${API_BASE_URL}/api/client/seats`,
   paymentCreateSession: `${API_BASE_URL}/payments/create-checkout-session`,
   showtimesInCinema: `${API_BASE_URL}/api/showtimes`,
-}
+};
