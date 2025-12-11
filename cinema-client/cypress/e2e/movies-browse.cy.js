8e49eb1b1a074035396e110a1c19638b88f49a7c
Ala Williams <alicja.m.williams@gmail.com>
2025-12-11 19:31:43 +0200

    updated url

--- cinema-client/cypress/e2e/movies-browse.cy.js
+++ cinema-client/cypress/e2e/movies-browse.cy.js
@@ -1,6 +1,6 @@
 describe("Browse Movies Feature", () => {
   it("allows a user to view the movie list", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("https://orange-wave-0372a7903.3.azurestaticapps.net/");
 
     cy.contains("Movies").click();
     cy.url().should("include", "/movies");
